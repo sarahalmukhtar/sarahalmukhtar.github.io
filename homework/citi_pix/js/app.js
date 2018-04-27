89cbb3f14d0cb2ab18e608f1f2bb6d99470078bb
sarahalmukhtar <sarah.almukhtar@nytimes.com>
2018-04-26 20:35:39 -0400

    citi pix

--- homework/citi_pix/js/app.js
+++ homework/citi_pix/js/app.js
@@ -1,3 +1,11 @@
 // As a user
 // When I change my selection
 // I expect the background image to change into the image of my selection
+
+$("#city-type").change(changeImg);
+
+function changeImg() {
+	var newClass = $("#city-type").val();
+	$("body").removeClass();
+	$("body").addClass(newClass);
+}
